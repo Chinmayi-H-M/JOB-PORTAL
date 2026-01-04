9d7d6221dc1719f0f522eacdf6a388548135f4a7
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-04 11:01:37 +0530

    Modelspy added for jobs

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+class Job(models.Model):
+    employer = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        limit_choices_to={'role': 'employer'}
+    )
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    requirements = models.TextField()
+    location = models.CharField(max_length=100)
+    job_type = models.CharField(max_length=50)
+    deadline = models.DateField()
+    is_filled = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Application(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    applicant = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        limit_choices_to={'role': 'jobseeker'}
+    )
+    applied_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50, default='Applied')
+
+    def __str__(self):
+        return f"{self.applicant.username} → {self.job.title}"
+
