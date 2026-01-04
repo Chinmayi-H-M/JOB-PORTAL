e1c808de8e2fd2df73084f4a36c3bb75084f5cc9
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-04 11:12:40 +0530

    Admin updated

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import User, EmployerProfile, JobSeekerProfile
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'role', 'is_staff', 'is_active')
+    list_filter = ('role', 'is_staff', 'is_active')
+    search_fields = ('username', 'email')
+
+
+@admin.register(EmployerProfile)
+class EmployerProfileAdmin(admin.ModelAdmin):
+    list_display = ('company_name', 'user')
+    search_fields = ('company_name',)
+
+
+@admin.register(JobSeekerProfile)
+class JobSeekerProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'location')
+    search_fields = ('user__username', 'location')
+
